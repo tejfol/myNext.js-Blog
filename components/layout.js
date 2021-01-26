a7b78dd0d36def97e8f613df299a15f1fb9817e5
tejfol <tejfolcss@gmail.com>
2021-01-26 04:13:03 +0200

    Done

--- components/layout.js
+++ components/layout.js
@@ -4,7 +4,7 @@ import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
 const name = 'Mark Matei'
-export const siteTitle = 'Next.js Sample Website'
+export const siteTitle = 'Mark Matei`s website'
 
 export default function Layout({ children, home }) {
   return (
